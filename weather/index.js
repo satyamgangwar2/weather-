d4f3cc3c07831a2159ed493c54c10d2b0a927832
satyamgangwar2 <gangwarsatyam30@gmail.com>
2024-07-09 10:58:51 +0530

    Add files via upload

---
+++ weather/index.js
@@ -0,0 +1,28 @@
+var inputvalue = document.querySelector('#cityinput');
+var btn = document.querySelector('#submit'); // Changed to match the corrected HTML id
+var city = document.querySelector('#cityoutput');
+var descrip = document.querySelector('#description');
+var temp = document.querySelector('#temp');
+var wind = document.querySelector('#wind');
+const apik = "6871f5985d7a20fdbdf352c58ff35218";
+
+function conversion(val) {
+    return (val - 273.15).toFixed(2); // Changed to convert from Kelvin to Celsius accurately
+}
+
+btn.addEventListener('click', function() {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + inputvalue.value + '&appid=' + apik)
+        .then(res => res.json())
+        .then(data => {
+            var nameval = data.name;
+            var descrip = data.weather[0].description;
+            var tempature = data.main.temp;
+            var windspeed = data.wind.speed;
+
+            city.innerHTML = `Weather of <span>${nameval}</span>`;
+            temp.innerHTML = `Temperature: <span>${conversion(tempature)} °C</span>`;
+            description.innerHTML = `Sky Condition: <span>${descrip}</span>`;
+            wind.innerHTML = `Wind Speed: <span>${windspeed} km/h</span>`;
+        })
+        .catch(err => alert('You entered an incorrect city name.'));
+});
