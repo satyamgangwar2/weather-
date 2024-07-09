f0f4b8035591bc2a224405d716d9d51411018ed1
satyamgangwar2 <gangwarsatyam30@gmail.com>
2024-07-09 11:13:12 +0530

    Delete weather/portfolio.js

--- weather/portfolio.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Form submitted!');
-    // You can add more form submission handling here
-});
