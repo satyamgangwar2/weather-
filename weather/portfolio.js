70752fe07322d4665d493a891617d3c91bf08984
satyamgangwar2 <gangwarsatyam30@gmail.com>
2024-07-09 10:53:35 +0530

    Add files via upload

---
+++ weather/portfolio.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Form submitted!');
+    // You can add more form submission handling here
+});
